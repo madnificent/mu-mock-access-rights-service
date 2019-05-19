7f3203d62a69ffaf21514736d6ef109ab15a353b
Aad Versteden <madnificent@gmail.com>
2019-05-19 19:44:51 +0200

    Initial implementation
    There are some strange things in here, like the fact that we wrap the
    access rights in a body where we should not need to.  This is the
    state we came at during development and it feels sufficient for our
    current cases.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM semtech/mu-javascript-template:1.3.5
 MAINTAINER Aad Versteden <madnificent@gmail.com>
+
 # see https://github.com/mu-semtech/mu-javascript-template for more info
+
--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-# see https://github.com/mu-semtech/mu-javascript-template for more info
-
 import { app, query, errorHandler } from 'mu';
+import bodyParser from 'body-parser';
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
+// Also parse application/json as json
+app.use( bodyParser.json( { type: function(req) { return /^application\/json/.test( req.get('content-type') ); } } ) );
+
+// Respond to POST requests
+app.post('/', function( req, res ) {
+  let allowedGroups = req.body;
+
+  console.log( allowedGroups.body );
+
+  res
+    .set( 'MU-AUTH-ALLOWED-GROUPS', allowedGroups.body )
+    .status( 204 )
+    .send();
+});
